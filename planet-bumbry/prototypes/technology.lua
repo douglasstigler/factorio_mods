2703df15e6597c39c7a0dac8f2b6262e9e860ffd
DSlice14 <douglas@stigler.com>
2025-11-22 18:52:26 -0500

    feat: Add new technology for discovering planet Bumbry.

--- planet-bumbry/prototypes/technology.lua
+++ planet-bumbry/prototypes/technology.lua
@@ -1,32 +1,36 @@
 local util = require("util")
 
-data:extend({
-    {
-        type = "technology",
-        name = "planet-discovery-bumbry",
-        icons = util.technology_icon_constant_planet("__planet-bumbry__/graphics/technology/planet-discovery-bumbry.png"),
-        icon_size = 512, -- Using 512 because the source image is 512 (assumed from starmap usage)
-        essential = true,
-        effects = {
-            {
-                type = "unlock-space-location",
-                space_location = "bumbry",
-                use_icon_overlay_constant = true
-            }
+local technology = {
+    type = "technology",
+    name = "planet-discovery-bumbry",
+    icons = util.technology_icon_constant_planet("__planet-bumbry__/graphics/technology/planet-discovery-bumbry.png"),
+    icon_size = 512, -- Using 512 because the source image is 512 (assumed from starmap usage)
+    essential = true,
+    effects = {
+        {
+            type = "unlock-space-location",
+            space_location = "bumbry",
+            use_icon_overlay_constant = true
+        }
+    },
+    prerequisites = {
+        "space-science-pack"
+    },
+    unit = {
+        count = 1000,
+        ingredients = {
+            { "automation-science-pack", 1 },
+            { "logistic-science-pack",   1 },
+            { "chemical-science-pack",   1 },
+            { "space-science-pack",      1 }
         },
-        prerequisites = {
-            "space-science-pack"
-        },
-        unit = {
-            count = 1000,
-            ingredients = {
-                { "automation-science-pack", 1 },
-                { "logistic-science-pack",   1 },
-                { "chemical-science-pack",   1 },
-                { "space-science-pack",      1 }
-            },
-            time = 60
-        },
-        order = "ea[bumbry]"
-    }
-})
+        time = 60
+    },
+    order = "ea[bumbry]"
+}
+
+if settings.startup["require-gleba-before-planet-bumbry"].value then
+    table.insert(technology.unit.ingredients, { "agricultural-science-pack", 1 })
+end
+
+data:extend({ technology })
